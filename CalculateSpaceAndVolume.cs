29cfe7c1852dcbdf2c123ee747f13efc22e31e08
PC <nikoloz.de@gmail.com>
2023-04-21 18:48:15 +0400

    Add project files.

---
+++ CalculateSpaceAndVolume.cs
@@ -0,0 +1,15 @@
+﻿namespace OverloadedMethods
+{
+    public class CalculateSpaceAndVolume
+    {
+        public int Calculate(int length, int width)
+        {
+            return length * width;
+        }
+
+        public int Calculate(int length, int width, int height)
+        {
+            return length * width *height;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OverloadedMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateSpaceAndVolume calculateSpaceAndVolume = new CalculateSpaceAndVolume();
+
+            // Calculating Space with overload method
+            int space = calculateSpaceAndVolume.Calculate(2, 4);
+            Console.WriteLine("Space is " + space);
+            int volume = calculateSpaceAndVolume.Calculate(3, 5, 7);
+            Console.WriteLine("Volume is " + volume);
+            Console.ReadKey();
+        }
+    }
+}
